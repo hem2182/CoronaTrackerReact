4b4ebdbb3551193c96933e0176ea23dd5a630e53
hem2182 <hemant_2182_fresco@yahoo.com>
2020-04-13 19:50:22 +0530

    added axios, countup, chartjs dependencies and basic app structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
+
+// dependencies required.
+// 1. npm install --save axios react-chartjs-2 react-countup classnames
