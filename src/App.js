7817563d9d971cab7ca9e636a56388f72e7253d4
hem2182 <hemant_2182_fresco@yahoo.com>
2020-04-14 20:46:35 +0530

    A working COVID-19 application

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 
+import coronaImage from './images/covid19Img.png';
+
+// whenever you have index files, you don't need to specify index.
+// By default, react looks for index.js file if not specified.
+import { fetchData } from './api';
+
 class App extends React.Component {
+
+    state = {
+        // you could have used constructor instead of this but that's lengthy.
+        // using state automatically invokes the constructor in code behind.
+        data: {},
+        country: '',
+    }
+
+    async componentDidMount() {
+        // this is the best place to fetch data from service/api in a class based component.
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+    }
+
+    handleCountryChange = async (country) => {
+        // fetch the data first and then set the state.
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    }
+
     render() {
+
+        const { data, country } = this.state;
+
         return (
             <div className={styles.container}>
-                <Cards />
-                <CountryPicker />
-                <Chart />
+                <img className={styles.image} src={coronaImage} alt="COVID-19"></img>
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
             </div>
         )
     }
@@ -18,4 +48,7 @@ class App extends React.Component {
 export default App;
 
 // dependencies required.
-// 1. npm install --save axios react-chartjs-2 react-countup classnames
+// 1. npm install --save axios react-chartjs-2 react-countup classnames @material-ui/core chart.js
+// 2. we will only have App as class component and rest all are functional components with hooks.
+// 3. whenever a component becomes too complex, switch from functional to class components.
+// 4. use class components for things like async calls and with multiple use effects with the hooks.
